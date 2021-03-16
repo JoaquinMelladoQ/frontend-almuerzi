15561fbf9d7d851e37060d22af9f8c2ece82d2e1
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-16 12:37:28 -0300

    Adding styles and ListItem

--- screens/Meals.js
+++ screens/Meals.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
 
-export default () => {
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  list: {
+    alignSelf: 'stretch',
+  }
+})
+
+const data = [
+  { _id: 'lala', name: 'Churrasco', desc: 'Plate tipico, palta, mayonesa' },
+]
+
+
+const Meals = ({ navigation }) => {
   return (
-    <View>
-      <Text>Soy Meals</Text>
+    <View style={styles.container}>
+      <FlatList 
+        style={styles.list}
+        data={data}
+        keyExtractor={x => x._id}
+        renderItem={({ item }) =>
+          <ListItem 
+            onPress={() => navigation.navigate('Modal', { _id: item._id })}
+            name={item.name}
+          />}
+      />
     </View>
   )
 }
+
+Meals.navigationOptions = ({
+  title: 'Comidas disponibles',
+})
+
+export default Meals
