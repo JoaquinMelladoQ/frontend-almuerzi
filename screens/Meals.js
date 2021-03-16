b8776b84b015d355487cbc772dd84fc74d0045aa
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-16 13:02:54 -0300

    custom hook used to move logic relative to api call

--- screens/Meals.js
+++ screens/Meals.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
 import ListItem from '../components/ListItem';
+import useFetch from '../hooks/useFetch';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,24 +15,23 @@ const styles = StyleSheet.create({
   }
 })
 
-const data = [
-  { _id: 'lala', name: 'Churrasco', desc: 'Plate tipico, palta, mayonesa' },
-]
-
 
 const Meals = ({ navigation }) => {
+  const { loading, data: meals } = useFetch('https://serverless-nlm4it9xw-joaquinmelladoq.vercel.app/api/meals')
+}
   return (
     <View style={styles.container}>
+      {loading ? <Text>Cargando...</Text> : 
       <FlatList 
         style={styles.list}
-        data={data}
+        data={meals}
         keyExtractor={x => x._id}
         renderItem={({ item }) =>
           <ListItem 
             onPress={() => navigation.navigate('Modal', { _id: item._id })}
             name={item.name}
           />}
-      />
+      />}
     </View>
   )
 }
