9cfd2e7ac37c83bb221580d93aab6cea6b1ddfa1
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-18 11:55:11 -0300

    Register screen added with its route

--- screens/Register.js
+++ screens/Register.js
@@ -1,8 +1,36 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
 
-export default () => {
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal:  15,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    marginBottom: 10,
+    paddingHorizontal: 5,
+  }
+})
+
+export default ({ navigation }) => {
   return (
-    <Text>Register</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Registro</Text>
+      <TextInput style={styles.input}/>
+      <TextInput style={styles.input}/>
+      <Button title='Enviar' onPress={() => {}}/>
+      <Button title='Volver al inicio' onPress={() => navigation.navigate('Login')} />
+    </View>
   )
 }
