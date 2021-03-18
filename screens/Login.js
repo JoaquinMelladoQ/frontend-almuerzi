7bbc88c623e70e87b2b9b02dd2b39d4e507d872a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-18 11:44:04 -0300

    Working on routes from login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,36 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
 
-export default () => {
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal:  15,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    marginBottom: 10,
+    paddingHorizontal: 5,
+  }
+})
+
+export default ({ navigation }) => {
   return (
-    <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar sesion</Text>
+      <TextInput style={styles.input}/>
+      <TextInput style={styles.input}/>
+      <Button title='Iniciar sesion' onPress={() => {}}/>
+      <Button title='Registrarse' onPress={() => navigation.navigate('Register')} />
+    </View>
   )
 }
