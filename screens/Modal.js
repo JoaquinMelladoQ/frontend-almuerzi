8dd9c94742a7fdadd3132b860c8651720c8f9e90
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-18 08:32:24 -0300

    Working on modal screen

--- screens/Modal.js
+++ screens/Modal.js
@@ -3,10 +3,13 @@ import { View, Text } from 'react-native';
 import useFetch from '../hooks/useFetch';
 
 export default ({ navigation }) => {
-  const { loadung, data } = useFetch()
+  const id = navigation.getParam('_id')
+  const { loading, data } = useFetch(`https://serverless-nlm4it9xw-joaquinmelladoq.vercel.app/api/meals${id}`)
   return (
     <View>
-      <Text>Soy Modal</Text>
+      <Text>{data._id}</Text>
+      <Text>{data.name}</Text>
+      <Text>{data.desc}</Text>
     </View>
   )
 }
